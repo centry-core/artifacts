6d1abe36ef192cf03f52a63c285a458676ca1e1a
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2022-05-19 15:41:52 +0300

    Fix events

--- static/js/artifact.js
+++ static/js/artifact.js
@@ -1,10 +1,12 @@
-$("#buckets-list").on('load-success.bs.table', function(e, data, status, type) {
+$(document).on('vue_init', () => {
+    $("#buckets-list").on('load-success.bs.table', function(e, data, status, type) {
     $("#buckets-list").bootstrapTable('check', 0);
-})
+    })
 
-$("#buckets-list").on('check.bs.table', function(e, row, element) {
+    $("#buckets-list").on('check.bs.table', function(e, row, element) {
     $("#bucket-name").text(row.name);
     $("#artifact-list").bootstrapTable('refresh', {url: `/api/v1/artifacts/artifacts/${getSelectedProjectId()}/${row.name}`});
+    })
 })
 
 function allowDrop(event) {
