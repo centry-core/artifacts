4996a92d27101e2fb10b666121ae8fe0ef6bbc4c
Yury Miklashevich1 <yury_miklashevich1@epam.com>
2022-12-19 15:41:23 +0300

    Adding API for deleting single artifact

--- api/v1/artifact.py
+++ api/v1/artifact.py
@@ -1,5 +1,6 @@
 from flask import send_file
 from io import BytesIO
+from hurry.filesize import size
 
 from flask_restful import Resource
 # from ....shared.tools.minio_client import MinioClient
@@ -19,3 +20,9 @@ class API(Resource):
         project = self.module.context.rpc_manager.call.project_get_or_404(project_id=project_id)
         fobj = MinioClient(project).download_file(bucket, filename)
         return send_file(BytesIO(fobj), attachment_filename=filename)
+
+    def delete(self, project_id: int, bucket: str, filename: str):
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id=project_id)
+        c = MinioClient(project=project)
+        c.remove_file(bucket, filename)
+        return {"message": "Deleted", "size": size(c.get_bucket_size(bucket))}, 200
